1b329d59cb8d8313761196d7f62d0f8451994bdf
Xiaobang Sun <xiaobang.sun@foxmail.com>
2023-11-08 22:16:34 +0200

    modifed v6

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
     cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
+
+  it('navigation', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
