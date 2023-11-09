1ffff4207beb218a575660b8f9199341751e5409
Xiaobang Sun <xiaobang.sun@foxmail.com>
2023-11-09 17:47:36 +0200

    v11

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    // eslint-disable-next-line no-unused-vars
-    setupNodeEvents(on, config) {
-    },
-  },
-})
